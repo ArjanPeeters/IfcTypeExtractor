5ea850dd5394ebbfc79b12d2e8e4655f042a433a
Arjan Peeters <arjan.peeters@gmail.com>
2024-06-05 15:53:22 +0200

    Made an option for all parameters

--- IfcEntityExtractorToCSV.py
+++ IfcEntityExtractorToCSV.py
@@ -1,12 +1,9 @@
 import ifcopenshell
-from ifcopenshell.util.element import get_pset
-from ifcopenshell.util.selector import Selector
+from ifcopenshell.util.element import get_pset, get_psets
 from easygui import fileopenbox, filesavebox, enterbox
 import pandas as pd
 from pathlib import Path
 import sys
-
-Selector.get_class_selector()
 
 files = fileopenbox(msg="Selecteer een of meerdere Ifc files", filetypes=["*.ifc", "*.ifczip"], multiple=True)
 if files is None:
@@ -21,9 +18,12 @@ if ',' in list_of_types:
 else:
     list_of_types = [list_of_types]
 
-list_of_psets = enterbox(msg='Geef de naam van de pset(s) die opgehaald moeten worden. Gebruik een komma voor meerdere Psets', strip=False)
+list_of_psets = enterbox(msg='Geef de naam van de pset(s) die opgehaald moeten worden. Gebruik een komma voor meerdere Psets. Type All voor alle parameters', strip=False)
+print(list_of_psets)
 if ',' in list_of_psets:
     list_of_psets = list_of_psets.split(',')
+elif list_of_psets == "All":
+    pass
 else:
     list_of_psets = [list_of_psets]
 
@@ -35,20 +35,32 @@ for file_name in files:
         print('file:', file_name, 'elements:', len(elements))
         for element in elements:
             properties = {'NO_PSET': []}
-            for pset in list_of_psets:
-                pset_properties = get_pset(element=element, name=pset)
-                if type(pset_properties) != dict:
-                    properties['NO_PSET'].append(pset)
-                else:
-                    properties.update(pset_properties)
+            if list_of_psets == "All":
+                pset_properties = get_psets(element=element)
+                print(pset_properties)
+                for pset, props in pset_properties.items():
+                    for prop, value in props.items():
+                        if pset[:len(element.Name)] == element.Name:
+                            properties[prop] = value
+                        else:
+                            properties[f'{pset}-{prop}'] = value
+            else:
+                for pset in list_of_psets:
+                    pset_properties = get_pset(element=element, name=pset)
+                    print(pset, pset_properties)
+                    if type(pset_properties) != dict:
+                        properties['NO_PSET'].append(pset)
+                    else:
+                        properties.update(pset_properties)
+                properties['GUID'] = element.GlobalId
+                properties['Element_name'] = element.Name
+                properties['Element_badID'] = element.Tag
+
             properties['IfcFile_Name'] = Path(file_name).stem
-            properties['GUID'] = element.GlobalId
-            properties['Element_name'] = element.Name
-            properties['Element_badID'] = element.Tag
             list_of_elements.append(properties)
 
 csvfile = filesavebox(msg="CSV filenaam", filetypes=[".csv"], default='parameters.csv')
 
 df = pd.DataFrame.from_dict(list_of_elements)
-df.to_csv(csvfile)
+df.to_csv(csvfile, index_label='regel_nr')
 print(df)
